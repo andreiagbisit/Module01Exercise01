15fdb15bd1a1bdab4cc983ac0fac52136fdd1b45
User <agbisit.andreijoaqhimali@auf.edu.ph>
2024-08-21 13:47:00 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Module01Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SaveOrUpdateInfo(object sender, EventArgs e)
+        {
+            await DisplayAlert("User Registration Form", "Details successfully saved or updated!", "OK");
+        }
+
+        private void OnChangeBackgroundColor(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.DarkGreen;
+        }
+    }
+
+}
---
+++ WelcomeDate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module01Exercise01
+{
+    public class WelcomeDate : IMarkupExtension
+    {
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return $"Welcome, User! Today is {DateTime.Now:MMMM dd, yyyy}";
+
+        }
+    }
+}
